0bfa7cb94c6255397e697e698a85c1b0c3e980a4
paulosgio <paulosergiodosanjossilvafilho@gmail.com>
2025-10-06 18:01:06 -0300

    feat: função de logout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 export default function Layout() {
+
+    const navigate = useNavigate()
+
+    const logOut = ()=> {
+        localStorage.clear()
+        navigate("/")
+    }
+
     return(
         <>
         <header>
             <h1>E-commerce</h1>
+            <button onClick={logOut}>Sair</button>
         </header>
         <main>
             <Outlet/>
