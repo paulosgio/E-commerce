4c6e34a09c08c5b8782e24ab62df06fb86a3f9cc
paulosgio <paulosergiodosanjossilvafilho@gmail.com>
2025-10-07 18:46:43 -0300

    adicionando o <profile/> na tela de carrinho

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,5 +1,15 @@
+import { useAppSelector } from "../../hooks"
+import Profile from "../Profile"
+
 export default function Cart() {
+
+    const isAdmin = useAppSelector(param => param.auth.isAdmin)
+    const name = localStorage.getItem("name")
+
     return(
-        <h1>Cart</h1>
+        <>
+            <Profile name={name} role={isAdmin ? "Admin" : "User"}/>
+            <h1>Cart</h1>
+        </>
     )
 }
