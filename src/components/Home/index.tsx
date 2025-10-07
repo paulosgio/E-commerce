6f97b66e2d66f0e00b360ee3ab82a6a6c5d2a7b0
paulosgio <paulosergiodosanjossilvafilho@gmail.com>
2025-10-07 18:52:10 -0300

    renderizado produtos na tela home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom"
 import { useAppSelector } from "../../hooks"
 import Profile from "../Profile"
 
@@ -5,11 +6,25 @@ export default function Home() {
 
     const name = localStorage.getItem("name")
     const isAdmin = useAppSelector(param => param.auth.isAdmin)
+    const data = useAppSelector(param => param.products)
+    const navigate = useNavigate()
 
     return(
         <>
             <h1>Home</h1>
             <Profile name={name} role={ isAdmin ? "Admin" : "User" }/>
+            <ul>
+            {data.map((param, i)=> {
+                return(
+                    <li onClick={()=> navigate(`/home/${param.id}`)} key={i}>
+                        <h4>{param.name}</h4>
+                        <h4>{param.description}</h4>
+                        <h4>{param.price}</h4>
+                        <button>Adicionar ao carrinho</button>
+                    </li>
+                )
+            })}
+            </ul>
         </>
     )
 }
