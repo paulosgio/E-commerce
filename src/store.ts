45bf62bf91926d0604409c8af789589a400c10da
paulosgio <paulosergiodosanjossilvafilho@gmail.com>
2025-10-07 18:44:53 -0300

    adicionado productsSlice no store

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./features/authSlice"
+import productsSlice from "./features/productsSlice"
 
 const store = configureStore({
     reducer: {
         auth: authSlice,
+        products: productsSlice
     }
 })
 
