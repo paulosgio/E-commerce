9dfbdce1dbd3bb19e03523167ab03cc5deaf6ea8
paulosgio <paulosergiodosanjossilvafilho@gmail.com>
2025-10-09 18:08:40 -0300

    adicionado funcao de adicionar carrinho ao botao

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom"
-import { useAppSelector } from "../../hooks"
+import { useAppDispatch, useAppSelector } from "../../hooks"
 import Profile from "../Profile"
+import { addToCart } from "../../features/cartSlice"
 
 export default function Home() {
 
@@ -8,20 +9,27 @@ export default function Home() {
     const isAdmin = useAppSelector(param => param.auth.isAdmin)
     const data = useAppSelector(param => param.products)
     const navigate = useNavigate()
+    const dispatch = useAppDispatch()
 
     return(
         <>
             <h1>Home</h1>
             <Profile name={name} role={ isAdmin ? "Admin" : "User" }/>
             <ul>
-            {data.map((param, i)=> {
+            {data.map(({name, description, id, price}, i)=> {
                 return(
-                    <li onClick={()=> navigate(`/home/${param.id}`)} key={i}>
-                        <h4>{param.name}</h4>
-                        <h4>{param.description}</h4>
-                        <h4>{param.price}</h4>
-                        <button>Adicionar ao carrinho</button>
-                    </li>
+                        <li onClick={()=> navigate(`/home/${id}`)} key={i}>
+                            <h4>{name}</h4>
+                            <h4>{description}</h4>
+                            <h4>{price}</h4>
+                            <button onClick={()=> dispatch(addToCart({
+                                name,
+                                description,
+                                id,
+                                price,
+                                quantity: 1
+                            }))}>Adicionar ao carrinho</button>
+                        </li>
                 )
             })}
             </ul>
