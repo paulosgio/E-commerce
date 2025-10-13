cee33bd10abec9f36b101a7db4e4ebb34ebdbfb6
paulosgio <paulosergiodosanjossilvafilho@gmail.com>
2025-10-13 17:49:51 -0300

    adicionando confirmacao de duas etapas no checkout

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { checkout, removeFromCart } from "../../features/cartSlice"
 import { addPurchase } from "../../features/purchasedSlice"
 import { useAppDispatch, useAppSelector } from "../../hooks"
@@ -9,6 +10,7 @@ export default function Cart() {
     const name = localStorage.getItem("name")
     const cart = useAppSelector(param => param.cart) || []
     const dispatch = useAppDispatch()
+    const [modalActive, setModalActive] = useState<boolean>(false) 
 
     function handleCheckout() {
         dispatch(addPurchase(cart))
@@ -36,7 +38,17 @@ export default function Cart() {
                     <p>Sem itens</p>
                 )}
             </ul>
-            <button onClick={handleCheckout}>Checkout</button>
+            <button onClick={()=> setModalActive(true)}>Checkout</button>
+            {modalActive && (
+                <>
+                    <h2>tem certeza que deseja confirmar a compra?</h2>
+                    <button onClick={()=> {
+                        handleCheckout()
+                        setModalActive(false)
+                    }}>Sim</button>
+                    <button onClick={()=> setModalActive(false)}>Nao</button>
+                </>
+            )}
         </>
     )
 }
